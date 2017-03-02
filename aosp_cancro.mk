023a1e587130bdac5e749c949ed7e4db2ce55336
ShivamKumarJha <jha.shivam3@gmail.com>
2017-03-02 15:42:03 +0530

    cancro: AOSP-CAF bringup

--- aosp_cancro.mk
+++ aosp_cancro.mk
@@ -21,12 +21,12 @@ TARGET_SCREEN_WIDTH := 1080
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
-PRODUCT_NAME := lineage_cancro
+PRODUCT_NAME := aosp_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
