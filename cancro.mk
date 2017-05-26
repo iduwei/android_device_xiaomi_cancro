a268c31a9b7da775617dc7543cede8f3a256505e
DrRamm <kotsareu@gmail.com>
2017-05-26 20:47:33 +0300

    cancro: power: true thermal zones and tweaks
    Conflicts:
    	configs/changepowermode.sh
    
    Change-Id: I59acd451e5db9eee9b5ac46fb7778a92b1446270

--- cancro.mk
+++ cancro.mk
@@ -74,10 +74,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
-    power.cancro
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/changepowermode.sh:system/bin/changepowermode.sh
+    power.msm8974
 
 # Vibrator
 PRODUCT_PACKAGES += \
